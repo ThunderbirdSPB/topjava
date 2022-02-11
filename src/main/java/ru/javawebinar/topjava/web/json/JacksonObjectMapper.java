f05783a7df934007145a62e5716ec4d69d0f45b4
ThunderbirdSPB <puckpuck505@gmail.com>
2022-02-11 16:10:52 +0300

    Applied patch 7_14

--- src/main/java/ru/javawebinar/topjava/web/json/JacksonObjectMapper.java
+++ src/main/java/ru/javawebinar/topjava/web/json/JacksonObjectMapper.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
  * @link https://github.com/FasterXML/jackson-docs/wiki/JacksonHowToCustomSerializers
  */
 public class JacksonObjectMapper extends ObjectMapper {
+    private static final ObjectMapper MAPPER = new JacksonObjectMapper();
 
     /**
      * В конструкторе:
@@ -37,5 +38,9 @@ public class JacksonObjectMapper extends ObjectMapper {
         setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    public static ObjectMapper getMapper() {
+        return MAPPER;
     }
 }
