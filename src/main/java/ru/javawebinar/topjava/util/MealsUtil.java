c9d6df59bd2a882f2694c82ee2cc3cf9715369b9
ThunderbirdSPB <puckpuck505@gmail.com>
2022-02-22 10:51:59 +0300

    Added spring binding, patch 9_04

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 import java.util.Collection;
 
 public class MealsUtil {
-    public static final int DEFAULT_CALORIES_PER_DAY = 2000;
 
     public static final List<Meal> meals = Arrays.asList(
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
